35960d4350c2536028e0286a839bb577d278ac77
Steven Rostedt (Red Hat) <rostedt@goodmis.org>
2014-12-12 20:05:10 -0500

    UPSTREAM: tracing: Move enabling tracepoints to just after rcu_init()
    Enabling tracepoints at boot up can be very useful. The tracepoint
    can be initialized right after RCU has been. There's no need to
    wait for the early_initcall() to be called. That's too late for some
    things that can use tracepoints for debugging. Move the logic to
    enable tracepoints out of the initcalls and into init/main.c to
    right after rcu_init().
    
    This also allows trace_printk() to be used early too.
    
    Link: http://lkml.kernel.org/r/alpine.DEB.2.11.1412121539300.16494@nanos
    Link: http://lkml.kernel.org/r/20141214164104.307127356@goodmis.org
    
    Reviewed-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Suggested-by: Thomas Gleixner <tglx@linutronix.de>
    Tested-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>
    
    Bug: 31856701
    Change-Id: I4cf16958f1e7b3842465649328f98196e3be01da
    (cherry picked from commit 5f893b2639b21ffe6834b1aebba392c37d2b83f9)
    Signed-off-by: Paul Lawrence <paullawrence@google.com>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -7058,6 +7058,13 @@ out:
 	return ret;
 }
 
+void __init trace_init(void)
+{
+	tracer_alloc_buffers();
+	init_ftrace_syscalls();
+	trace_event_init();	
+}
+
 __init static int clear_boot_tracer(void)
 {
 	/*
@@ -7077,6 +7084,5 @@ __init static int clear_boot_tracer(void)
 	return 0;
 }
 
-early_initcall(tracer_alloc_buffers);
 fs_initcall(tracer_init_debugfs);
 late_initcall(clear_boot_tracer);

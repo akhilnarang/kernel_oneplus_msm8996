5567b4fde43ff201085b2986c234533ade98743f
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2016-01-07 16:46:14 +0100

    BACKPORT: PM / sleep: Go direct_complete if driver has no callbacks
    Backport notes: This resolves clk warnings in the designware i2c
    driver on HiKey seen during suspend/resume.
    
    Cherrypicked from: aa8e54b559479d0cb7eb632ba443b8cacd20cd4b
    
    If a suitable prepare callback cannot be found for a given device and
    its driver has no PM callbacks at all, assume that it can go direct to
    complete when the system goes to sleep.
    
    The reason for this is that there's lots of devices in a system that do
    no PM at all and there's no reason for them to prevent their ancestors
    to do direct_complete if they can support it.
    
    Change-Id: Ia773afb4b266f012336b99fc8cf87453839e078b
    Signed-off-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>
    Reviewed-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    [jstultz: Backported to 4.4]
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- drivers/base/power/power.h
+++ drivers/base/power/power.h
@@ -51,6 +51,7 @@ extern void device_pm_remove(struct device *);
 extern void device_pm_move_before(struct device *, struct device *);
 extern void device_pm_move_after(struct device *, struct device *);
 extern void device_pm_move_last(struct device *);
+extern void device_pm_check_callbacks(struct device *dev);
 
 #else /* !CONFIG_PM_SLEEP */
 
@@ -68,6 +69,8 @@ static inline void device_pm_move_before(struct device *deva,
 static inline void device_pm_move_after(struct device *deva,
 					struct device *devb) {}
 static inline void device_pm_move_last(struct device *dev) {}
+
+static inline void device_pm_check_callbacks(struct device *dev) {}
 
 #endif /* !CONFIG_PM_SLEEP */
 

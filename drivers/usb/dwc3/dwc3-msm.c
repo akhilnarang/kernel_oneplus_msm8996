90d4f9ac3b0bb984d9070c3fc05c9010ae7a79e3
Sultanxda <sultanxda@gmail.com>
2016-09-24 13:46:09 -0700

    usb: dwc3-msm: Remove indefinite wakeup
    Something is wrong with this wakeup; it isn't always relaxed, thus
    wreaking havoc on battery life. We already have a wakeup in smbcharger
    that is triggered on USB connection, so instead of fixing this one, just
    remove it since it's redundant.
    
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -2072,8 +2072,6 @@ static int dwc3_msm_suspend(struct dwc3_msm *mdwc)
 		dev_dbg(mdwc->dev, "defer suspend with %d(msecs)\n",
 					mdwc->lpm_to_suspend_delay);
 		pm_wakeup_event(mdwc->dev, mdwc->lpm_to_suspend_delay);
-	} else {
-		pm_relax(mdwc->dev);
 	}
 
 	atomic_set(&dwc->in_lpm, 1);
@@ -2109,8 +2107,6 @@ static int dwc3_msm_resume(struct dwc3_msm *mdwc)
 		dev_dbg(mdwc->dev, "%s: Already resumed\n", __func__);
 		return 0;
 	}
-
-	pm_stay_awake(mdwc->dev);
 
 	/* Enable bus voting */
 	if (mdwc->bus_perf_client) {
@@ -3062,7 +3058,6 @@ static int dwc3_msm_probe(struct platform_device *pdev)
 		register_cpu_notifier(&mdwc->dwc3_cpu_notifier);
 
 	device_init_wakeup(mdwc->dev, 1);
-	pm_stay_awake(mdwc->dev);
 
 	if (of_property_read_bool(node, "qcom,disable-dev-mode-pm"))
 		pm_runtime_get_noresume(mdwc->dev);
@@ -3563,7 +3558,6 @@ static void dwc3_otg_sm_work(struct work_struct *w)
 				dwc3_msm_gadget_vbus_draw(mdwc,
 						dcp_max_current);
 				atomic_set(&dwc->in_lpm, 1);
-				pm_relax(mdwc->dev);
 				break;
 			case DWC3_CDP_CHARGER:
 			case DWC3_SDP_CHARGER:

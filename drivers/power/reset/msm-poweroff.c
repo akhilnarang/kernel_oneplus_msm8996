9241f57a7cf6924c315d00bfa619019910b9ba47
jiachunxu <leo.jia@oneplus.net>
2017-01-20 16:00:50 +0800

    power: add oneplus reboot commands
    Change-Id: I87fbf0ad807c254b9aa22882eb93e4c3b102db12

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -336,8 +336,14 @@ static void msm_restart_prepare(const char *cmd)
 		} else if (!strncmp(cmd, "edl", 3)) {
 			enable_emergency_dload_mode();
 		} else {
+			qpnp_pon_set_restart_reason(
+				PON_RESTART_REASON_REBOOT);
 			__raw_writel(0x77665501, restart_reason);
 		}
+	} else {
+		qpnp_pon_set_restart_reason(
+			PON_RESTART_REASON_REBOOT);
+		__raw_writel(0x77665501, restart_reason);
 	}
 
 	flush_cache_all();
